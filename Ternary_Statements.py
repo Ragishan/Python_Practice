337c512541f05156a524ecdc132e8fb381c18fd2
Raghav Shandilya <ragishandilya@gmail.com>
2022-09-08 11:46:05 +0530

    typo changes made to ternary_statements.py

--- Ternary_Statements.py
+++ Ternary_Statements.py
@@ -1,4 +1,4 @@
 
-#WAP to check wherther the number entered is smaller, greater or equal to 10 using ternary statements 
+#WAP to check whether the number entered is smaller, greater or equal to 10 using ternary statements 
 n = int(input())
 print("Smaller") if n < 10  else print("Equal") if n == 10 else print("Greater") 
