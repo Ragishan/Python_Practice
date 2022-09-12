10073aee114758a09d2371bb9dc22157283e8cdb
Raghav Shandilya <ragishandilya@gmail.com>
2022-09-12 12:28:02 +0530

    Class Obj example

--- extractingstuff_fromastring.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def repeatedchar(self, s: str) -> str:
-        occurences = defaultdict(int)
-        for char in s:
-            occurences[char] += 1
-            if occurences[char] == 2:
-                return char
-    
-    repeatedchar("awccx")
